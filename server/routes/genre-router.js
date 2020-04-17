764ccd787dc224d02db36a1c5d0a961a53ed521c
Ashley Scott <aescott87@gmail.com>
2020-04-17 16:38:09 -0500

    Set up GET Saga for requesting movie data from DB. Rendered movie data to home page

--- server/routes/genre-router.js
+++ server/routes/genre-router.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
     pool.query(queryText)
       .then((result) => { res.send(result.rows); })
       .catch((error) => {
-        console.log('Error completing SELECT movies query', error);
+        console.log('Error completing SELECT genres query', error);
         res.sendStatus(500);
     });
 });
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  getMovies = () => {
+    this.props.dispatch({type: 'GET_MOVIES'})
+  }
+
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <div className="app-header">
+          <header>
+            <h1>Movie Collection</h1>
+          </header>
+        </div>
+        <div className="movies-list">
+          <ul>
+            {this.props.movies.map (item => (
+              <li key={item.id}>
+                <h2>{item.title}</h2>
+                <img src={item.poster} alt={`Poster for ${item.title}`}/>
+                <p>{item.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (reduxStore) => ({
+  movies: reduxStore.movies
+  
+})
+
+export default connect(mapStateToProps)(App);
