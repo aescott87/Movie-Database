3e1a30fe3856ad6453961f182224adb0e6fac6f3
Ashley Scott <aescott87@gmail.com>
2020-04-17 14:36:47 -0500

    Set up routers to get movies and genres from the database

---
+++ server/routes/genre-router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+//GET all genres from the DB
+router.get('/', (req, res) => {
+    const queryText = 'SELECT * FROM genres';
+    pool.query(queryText)
+      .then((result) => { res.send(result.rows); })
+      .catch((error) => {
+        console.log('Error completing SELECT movies query', error);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+//Adding routes
+const movieRouter = require('./routes/movie-router');
+const genreRouter = require('./routes/genre-router');
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/movies', movieRouter);
+app.use('/genres', genreRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
